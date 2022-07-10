b183239f4139a1f0156ee7872b2fb46a00230ee5
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-10 13:21:53 -0400

    create stylesheet and uncomment on index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from "react-dom";
-// import "./css/style.css";
+import "./css/style.css";
 import App from './components/App';
 
 render(<App />, document.querySelector("#main"));
