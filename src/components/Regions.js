e67e212752812ed8af1c963515dcf93fd16f108f
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-10 13:26:28 -0400

    add div for regions component with class of regions

--- src/components/Regions.js
+++ src/components/Regions.js
@@ -3,7 +3,10 @@ import React from "react";
 class Regions extends React.Component {
     render() {
         return (
-            <p>Regions!</p>
+            <div className="regions">
+                <p>Regions!</p>
+            </div>
+            
         )
     }
 }
