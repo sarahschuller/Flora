6b8f3fd5b6ef634d75e5bfa5ae7b6a0894634e47
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-10 15:39:24 -0400

    create sample data for each region, populating plants for ne-plants

---
+++ src/ne-plants.js
@@ -0,0 +1,59 @@
+const nePlants = {
+
+    plant1: {
+        name: "Arugula",
+        image: "/images/arugaula.jpg",
+        desc: "Fresh arugula leaves have a distinctive spicy kick that will turn up flavors in your salads, pastas, sandwiches, and sauces.",
+        months: {"March", "April", "May", "June", "July", "August"} ,
+    }
+
+    plant2: {
+        name: "Carrosts",
+        image: "/images/carrots.jpg",
+        desc: ,
+        months: {"March", "April", "May", "June", "July", "August"},
+    }
+
+    plant3: {
+        name: "Onions",
+        image: "/images/onions.jpg",
+        desc: ,
+        months: {"March", "April", "May"},
+    }
+
+    plant4: {
+        name: ,
+        image: ,
+        desc: ,
+        months: ,
+    }
+
+    plant5: {
+        name: ,
+        image: ,
+        desc: ,
+        months: ,
+    }
+
+    plant6: {
+        name: ,
+        image: ,
+        desc: ,
+        months: ,
+    }
+
+    plant7: {
+        name: ,
+        image: ,
+        desc: ,
+        months: ,
+    }
+
+    plant8: {
+        name: ,
+        image: ,
+        desc: ,
+        months: ,
+    }
+
+}
