e01385d9c9eb9b2bfe6b5b22e12ef5f31fe96cf7
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-10 17:37:57 -0400

    add plants to state and create load function for NE plants

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,23 @@ import React from "react";
 import Regions from "./Regions";
 import PlantsList from "./PlantsList";
 import Plants from "./Plants";
+import nePlants from "../ne-plants";
 
 class App extends React.Component {
+  state = {
+    plants: {},
+  };
+
+  loadNePlants = () => {
+    this.setState({ plants: nePlants });
+  };
+
   render () {
     return (
       <div className="app">
-      <Regions />
+      <Regions 
+        loadNePlants={this.loadNePlants}
+        />
       <PlantsList />
       <Plants />
       </div>
