2bd6e3757bf35fc9fb11674d3f8760f53c73455c
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-10 15:50:47 -0400

    update

--- src/ne-plants.js
+++ src/ne-plants.js
@@ -4,56 +4,56 @@ const nePlants = {
         name: "Arugula",
         image: "/images/arugaula.jpg",
         desc: "Fresh arugula leaves have a distinctive spicy kick that will turn up flavors in your salads, pastas, sandwiches, and sauces.",
-        months: {"March", "April", "May", "June", "July", "August"} ,
-    }
+        months: ["March", "April", "May", "June", "July", "August"] ,
+    },
 
     plant2: {
         name: "Carrots",
         image: "/images/carrots.jpg",
         desc: "Herbaceous, generally biennial plant of the Apiaceae family that produces an edible taproot. Among common varieties root shapes range from globular to long, with lower ends blunt to pointed. Besides the orange-coloured roots, white-, yellow-, and purple-fleshed varieties are known.",
-        months: {"March", "April", "May", "June", "July", "August"},
-    }
+        months: ["March", "April", "May", "June", "July", "August"],
+    },
 
     plant3: {
         name: "Onions",
         image: "/images/onions.jpg",
         desc: "A round vegetable with a light brown skin. It has many white layers on its inside which have a strong, sharp smell and taste.",
-        months: {"March", "April", "May"},
-    }
+        months: ["March", "April", "May"],
+    },
 
     plant4: {
         name: "Peas",
         image: "/images/peas.jpg",
         desc: "A most commonly green, occasionally golden yellow, or infrequently purple pod-shaped vegetable, widely grown as a cool-season vegetable crop.",
-        months: {"March", "April", "May", "July", "August"},
-    }
+        months: ["March", "April", "May", "July", "August"],
+    },
 
     plant5: {
         name: "Broccoli",
         image: "/images/broccoli.jpg",
         desc: "An herbaceous annual or biennial grown for its edible flower heads which are used as a vegetable. The broccoli plant has a thick green stalk, or stem, which gives rise to thick, leathery, oblong leaves which are gray-blue to green in color.",
-        months: {"April", "May", "June", "July", "August"},
-    }
+        months: ["April", "May", "June", "July", "August"],
+    },
 
     plant6: {
         name: "Corn",
         image: "/images/corn.jpg",
         desc: "A tall annual cereal grass (Zea mays) that is widely grown for its large elongated ears of starchy seeds. The seeds, which are also known as corn, are used as food for humans and livestock and as a source of biofuel and can be processed into a wide range of useful chemicals.",
-        months: {"May", "June", "July"},
-    }
+        months: ["May", "June", "July"],
+    },
 
     plant7: {
         name: "Pumpkins",
         image: "/images/pumpkin.jpg",
         desc: "Pumpkins are often yellowish to orange in colour, and they vary from oblate to globular to oblong; some feature a white rind. The rind is smooth and usually lightly furrowed or ribbed. The fruit stem is hard and woody, ridged, and angled.",
-        months:{"May", "June", "July"} ,
-    }
+        months:["May", "June", "July"] ,
+    },
 
     plant8: {
         name: "Peppers",
         image: "/images/peppers.jpg",
         desc: "The pepper is a vegetable with variable shape, size and colour. It can be green, red, yellow, orange and even black!. Its taste can be sweet or spicy and it is consumed fresh, preserve, etc. The pepper is consumed raw, stewed and roasted or as garnish in a great variety of dishes.",
-        months: {"May", "June"},
-    }
+        months: ["May", "June"],
+    },
 
 }
