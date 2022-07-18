e63530be55a4a9d6dfcfb0345145484237ce008a
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-18 16:07:03 -0400

    update className

--- src/components/App.js
+++ src/components/App.js
@@ -31,7 +31,7 @@ class App extends React.Component {
 
   render () {
     return (
-      <div className="app">
+      <div className="wrapper">
         <div className="region-selection">
       <Header />
       <Regions 
